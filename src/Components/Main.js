542b5b53e1aaa4d4ec3993ef2fd1b20580b4aadb
Caleb <calebcoecode@gmail.com>
2020-07-29 00:11:24 -0600

    Updated React Router path

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,7 +7,7 @@ import { Switch, Route } from 'react-router-dom';
 
 const Main = () => (
     <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/Caleb_React_Website" component={Home} />
         <Route path="/Projects" component={Projects} />
         <Route path="/Contact" component={Contact} />
     </Switch>
